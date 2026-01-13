2a78e8e26c684dbd5fe28492d8b6b8349eb72e0f
valerofin <valerofin.111@gmail.com>
2026-01-13 05:17:06 +0300

    Updates

---
+++ assets/ModalView-BQndZnoJ.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index-BPKpPUjb.js";import{V as e}from"./View-NiJ-jASe.js";var i=()=>t.jsx(e,{nextPage:"Switch",nextPageUrl:"/Switch",title:"Modal",children:"Modal"});export{i as default};
