8ff7ba504cebd6bcaa631050863b26e80977ffcc
valerofin <valerofin.111@gmail.com>
2026-01-13 05:25:07 +0300

    Updates

--- assets/ModalView-BQndZnoJ.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./index-BPKpPUjb.js";import{V as e}from"./View-NiJ-jASe.js";var i=()=>t.jsx(e,{nextPage:"Switch",nextPageUrl:"/Switch",title:"Modal",children:"Modal"});export{i as default};
---
+++ assets/ModalView-dMzIz2xD.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index-CCMvXYeK.js";import{V as e}from"./View-EmdayNun.js";var i=()=>t.jsx(e,{nextPage:"Switch",nextPageUrl:"/Switch",title:"Modal",children:"Modal"});export{i as default};
